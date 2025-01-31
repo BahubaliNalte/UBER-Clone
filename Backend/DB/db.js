34624ea6a20d93f3fc1c13839750c4bc4ac85780
BahubaliNalte <bahubalidnalte722006@gmail.com>
2025-01-31 10:23:56 +0530

    complete backend and frontend

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -1,20 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const uri = process.env.MONGODB_URI;  // Ensure this is being pulled from your .env file
-    if (!uri) {
-      console.error("MongoDB URI is not defined in .env file");
-      process.exit(1);
-    }
 
-    // Remove the deprecated options
-    await mongoose.connect(uri);
-    console.log("DataBase Connected");
-  } catch (err) {
-    console.error("DataBase connection failed", err);
-    process.exit(1);
-  }
-};
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+        console.log('Connected to DB');
+    }).catch(err => console.log(err));
+}
 
-module.exports = connectDB;
+
+module.exports = connectToDb;
